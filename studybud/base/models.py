9600b3cceb9c66e348d097c3cb5fa6aeeba22978
zgurley1 <zgurley1@charlotte.edu>
2024-09-08 17:58:50 -0400

    Database and Admin Panel

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Room(models.Model):
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200)
+    #setting null to true means that the field is allowed to be blank, allowing for updating later
+    description = models.TextField(null=True, blank=True)
+    #participants =
+    #Everytime the save method is called it saves a timestamp
+    updated = models.DateTimeField(auto_now=True)
+    #auto now add only takes the timestamp when the initial instance is created
+    created = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.name)
+    
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    #When the parent is deleted, delete all the messages in the room
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body[0:50]
--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,23 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 
-
-
-rooms = [
+"""rooms = [
     {'id':1, 'name':'Lets learn python!'},
     {'id':2, 'name':'Design with me'},
     {'id':3, 'name':'Frontend Developers'},
 ]
+"""
+
 # Create your views here.
 #create a view
 def home(request):
+    #Gets all the rooms in the database
+    rooms = Room.objects.all()
     context = {'rooms':rooms}
     return render(request, 'base/home.html', context)
 
+
+
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
